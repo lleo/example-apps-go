6ebb29b89a7a5ce1f1f42710cb7df38c797d6155
LLeo <lleoem@gmail.com>
2019-01-12 17:07:04 -0500

    first working client and server for grpc Get() any key with empty go-functional-collections/fmap

--- key-val-network-app/client/main.go
+++ key-val-network-app/client/main.go
@@ -8,29 +8,61 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"strings"
+	"context"
 
+	"github.com/pkg/errors"
 	flatbuffers "github.com/google/flatbuffers/go"
+	//"github.com/lleo/example-apps-go/key-val-network-app/keyval"
 	"github.com/lleo/example-apps-go/key-val-network-app/keyval"
 	"google.golang.org/grpc"
 )
 
-const defaultPort = "9090"
-const defaultIp = "localhost"
+ const defaultPort = "9090"
+const defaultIP = "localhost"
+
+func usage(out io.Writer, xit int, msg string) {
+	fmt.Fprintln(out, msg)
+	fmt.Fprintln(out, "go <cmd> [<cmd-arg>*]")
+	fmt.Fprintln(out, "  ex#1 $ cli get \"key\"")
+	fmt.Fprintln(out, "  ex#2 $ cli put \"key\" \"val\"")
+	os.Exit(xit)
+}
 
 func main() {
 	fmt.Println("len(os.Args) =", len(os.Args))
 	fmt.Println("os.Args =", os.Args)
 
 	var addr string
-	flags.StringVar(&addr, "a", defaultIp+":"+defaultPort,
-		"(hostname|ip):port (eg \"localhost:9090\")")
-	addr = defaultIp + ":" + defaultPort
-	if len(os.Args) > 2 {
-		addr = os.Args[1] + ":" + os.Args[2]
+	//flags.StringVar(&addr, "a", defaultIp+":"+defaultPort,
+	//	"(hostname|ip):port (eg \"localhost:9090\")")
+	addr = defaultIP + ":" + defaultPort
+
+	if len(os.Args) < 2 {
+		usage(os.Stdout, 0, "")
 	}
 
-	var conn grpc.ClientConn
+	b := flatbuffers.NewBuilder(0)
+
+	cmd := strings.ToLower(os.Args[1])
+	var key string
+	//var val string
+	switch cmd {
+	case "get":
+		if len(os.Args) != 3 {
+			usage(os.Stderr, 1, "args != 3")
+		}
+		key=os.Args[2] // $ cli[0] get[1] "value"[2]
+	case "put":
+		usage(os.Stderr, 1, "\"put\" not implemented.")
+	case "getkeys":
+		usage(os.Stderr, 1, "\"getkeys\" not implemented.")
+	default:
+	}
+
+	var conn *grpc.ClientConn
 	var err error
 	conn, err = grpc.Dial(
 		addr,
@@ -38,19 +70,43 @@ func main() {
 		grpc.WithCodec(flatbuffers.FlatbuffersCodec{}),
 	)
 	if err != nil {
-		fmt.Panicf("grpc.Dial failed: %s\n", err)
+		panic(fmt.Errorf("grpc.Dial failed: %s", err))
 	}
 	defer conn.Close()
 
 	client := keyval.NewKeyValSvcClient(conn)
 
 	switch cmd {
-	case "put":
 	case "get":
+		//build request
+
+		//must do b.CreateString() outside of keyval.*Start() and
+		//keyval.*Finish() calls or else I get "Incorrect creation order:
+		//object must /not be nested." error panic from keyval library.
+		reqKey := b.CreateString(key)
+
+		keyval.GetReqStart(b)
+		keyval.GetReqAddKey(b, reqKey)
+		b.Finish(keyval.GetReqEnd(b))
+
+		//client call
+		var rsp *keyval.GetRsp;
+		fmt.Printf("Sending Get(%q)\n", key)
+		rsp, err = client.Get(context.Background(), b)
+		if err != nil {
+			panic(errors.Wrap(err, "Get grpc request failed"))
+		}
+
+		fmt.Printf("requested key=%q\n", key)
+		fmt.Printf("responsed val=%q\n", rsp.Val())
+	case "put":
+		panic(fmt.Errorf("%q cmd not implemented", cmd))
+		//var rsp *keyval.PutRsp
+		//rsp, err = client.Put(context.Background(), b)
 	case "keys":
-		fmt.Panicf("%q cmd not implemented\n", cmd)
+		panic(fmt.Errorf("%q cmd not implemented", cmd))
 	default:
-		fmt.Panicf("unknown cmd: %q\n", cmd)
+		panic(fmt.Errorf("unknown cmd: %q", cmd))
 	}
 	fmt.Println("done.")
 }
--- key-val-network-app/server/main.go
+++ key-val-network-app/server/main.go
@@ -4,36 +4,114 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 
+	//"flags"
+	//"gopkg.in/urfave/cli.v2" // imports as package "cli"
+	//"github.com/lleo/go-functional-collections/fmap"
+	//"github.com/lleo/go-functional-collections/key"
+	"github.com/lleo/go-functional-collections/fmap"
+	"github.com/lleo/go-functional-collections/key"
+
 	"google.golang.org/grpc"
+	//"github.com/lleo/example-apps-go/key-val-network-app/keyval"
 
 	flatbuffers "github.com/google/flatbuffers/go"
+	"github.com/lleo/example-apps-go/key-val-network-app/keyval"
 )
 
 const defaultPort = 6060
 const defaultIP = "localhost"
 
+type server struct{}
+
+var m *fmap.Map
+
+func (s *server) Put(
+	ctx context.Context,
+	req *keyval.PutReq) (*flatbuffers.Builder, error) {
+	log.Printf("Put called: key=%q; val=%q\n",
+		req.Key(), req.Val())
+
+	var added bool
+	m, added = m.Store(key.Str(req.Key()), req.Val())
+
+	log.Printf("Sending PutRsp(added=%t)\n", added)
+
+	b := flatbuffers.NewBuilder(0)
+
+	keyval.PutRspStart(b)
+	//Should be:
+	//keyval.PutRspAddAdded(b, added)
+	if added {
+		keyval.PutRspAddAdded(b, 1)
+	} else {
+		keyval.PutRspAddAdded(b, 0)
+	}
+
+	b.Finish(keyval.PutReqEnd(b))
+
+	return b, nil
+}
+
+func (s *server) Get(ctx context.Context,
+	req *keyval.GetReq) (*flatbuffers.Builder, error) {
+	log.Printf("Get called: key=%q\n", req.Key())
+
+	val, found := m.Load(key.Str(req.Key()))
+
+	log.Printf("Sending GetRsp(val=%q, found=%t)\n", val, found)
+
+	b := flatbuffers.NewBuilder(0)
+	var rspVal flatbuffers.UOffsetT
+	if val != nil {
+		rspVal = b.CreateString(val.(string))
+	} else {
+		rspVal = b.CreateString("empty")
+	}
+
+	keyval.GetRspStart(b)
+	keyval.GetRspAddVal(b, rspVal)
+	//should be:
+	//keyval.GetRspAddExists(b, found)
+	if found {
+		keyval.GetRspAddExists(b, 1)
+	} else {
+		keyval.GetRspAddExists(b, 0)
+	}
+	b.Finish(keyval.PutRspEnd(b))
+
+	return b, nil
+}
+
 func main() {
 	fmt.Println("len(os.Args) =", len(os.Args))
 	fmt.Println("os.Args =", os.Args)
 	fmt.Print("Starting...")
-	svrSk, err := net.Listen("tcp", addr)
+
+	addr := "0.0.0.0:9090"
+	var svrSk, err = net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
-	gSvr := grpc.NewServer(grpc.CustomCodec(
+	m = fmap.New()
+
+	grpcSvr := grpc.NewServer(grpc.CustomCodec(
 		flatbuffers.FlatbuffersCodec{}))
 
-	//register Service
-	svr := gSvr.Serve(svrSk)
+	keyval.RegisterKeyValSvcServer(grpcSvr, &server{})
 
-	if err := svr.Serve(lis); err != nil {
+	fmt.Println("started.")
+
+	//register Service
+	err = grpcSvr.Serve(svrSk)
+
+	if err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
-	fmt.Println("started.")
 }
